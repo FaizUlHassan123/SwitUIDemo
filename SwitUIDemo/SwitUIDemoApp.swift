5e8567625cbcf625bd84614897b3e12806194e17
uzair <uzair.ikram@sdsoltechnologies.com>
2022-07-22 20:09:05 +0500

    Initial Commit

---
+++ SwitUIDemo/SwitUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitUIDemoApp.swift
+//  SwitUIDemo
+//
+//  Created by Faiz Ul Hassan on 22/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwitUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
